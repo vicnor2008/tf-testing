0e6fb014347815cddbfc76a8c1d7ec266c9e6f9e
vicnor <valenciaram@gmail.com>
2024-06-10 23:18:41 -0600

    commit3

--- main.tf
+++ main.tf
@@ -1,51 +1,51 @@
-## PUT TERRAFORM CLOUD BLOCK HERE!  ##
+# ## PUT TERRAFORM CLOUD BLOCK HERE!  ##
 
-terraform {
+# terraform {
 
-  cloud {
-    organization = "viccompany"
+#   cloud {
+#     organization = "viccompany"
 
-    workspaces {
-      name = "tf-cloud-test"
-    }
-  }
+#     workspaces {
+#       name = "tf-cloud-test"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.01"
-    }
-  }
-
-
-
-}
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.01"
+#     }
+#   }
 
 
-# Variable blocks directly within the main.tf. No arguments necessary.
-# variable "aws_access_key" {}
-# variable "aws_secret_key" {}
-variable "region" {}
 
-# provider arguments call on the variables which then call on terraform.tfvars for the values.
-provider "aws" {
-  # access_key = var.aws_access_key
-  # secret_key = var.aws_secret_key
-  region     = var.region
-}
+# }
 
-# Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# # Variable blocks directly within the main.tf. No arguments necessary.
+# # variable "aws_access_key" {}
+# # variable "aws_secret_key" {}
+# variable "region" {}
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+# # provider arguments call on the variables which then call on terraform.tfvars for the values.
+# provider "aws" {
+#   # access_key = var.aws_access_key
+#   # secret_key = var.aws_secret_key
+#   region     = var.region
+# }
 
-resource "aws_iam_user" "test_user_99" {
-  name = "test-user-99"
+# # Add .gitignore file in this directory with the terraform.tfvars
+
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
+
+# resource "aws_iam_user" "test_user_99" {
+#   name = "test-user-99"
   
-}
+# }
