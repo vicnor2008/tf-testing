08ef4a832f778294201bf2cefc029e701436a924
vicnor <valenciaram@gmail.com>
2024-06-10 23:15:50 -0600

    commit2

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "tc_instance" {
   }
 }
 
-# resource "aws_iam_user" "test_user_99" {
-#   name = "test-user-99"
+resource "aws_iam_user" "test_user_99" {
+  name = "test-user-99"
   
-# }
+}
